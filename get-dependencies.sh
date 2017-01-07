ee74feaf7d953d3b284d239d9e2c19b050c9b883
juju4 <juju4@users.noreply.github.com>
2017-01-07 04:57:30 -0500

    sync repositories - galaxy naming

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -5,5 +5,11 @@ umask 022
 
 rolesdir=$(dirname $0)/..
 
-[ ! -d $rolesdir/redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/redhat-epel
+[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
+## galaxy naming: kitchen fails to transfer symlink folder
+#[ ! -e $rolesdir/juju4.mig-postgres ] && ln -s ansible-mig-postgres $rolesdir/juju4.mig-postgres
+[ ! -e $rolesdir/juju4.mig-postgres ] && cp -R $rolesdir/ansible-mig-postgres $rolesdir/juju4.mig-postgres
+
+## don't stop build on this script return code
+true
 
